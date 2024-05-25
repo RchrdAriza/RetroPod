a23d433e429fed5c25b0fa2d2034b79d75de8334
RchrdAriza <rchrdalv@gmail.com>
2024-05-24 21:41:22 -0500

    construi el esqueleto basico

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RetroPod',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        body: BodyApp(),
+      ),
+    );
+  }
+}
+
+class BodyApp extends StatelessWidget {
+  const BodyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Container(
+          color: const Color.fromRGBO(119, 119, 119, 1),
+          child: const Column(
+            children: [
+              Expanded(
+                  flex: 5,
+                  child: SizedBox(
+                    height: double.infinity,
+                    width: double.infinity,
+                  )),
+              SizedBox(
+                height: 1,
+              ),
+              Expanded(
+                  flex: 4,
+                  child: SizedBox(
+                    height: double.infinity,
+                    width: double.infinity,
+                  )),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
